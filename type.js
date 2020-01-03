2a95ff17de19e4dd3555733d7d980a84622ccfb0
jauntywunderkind <jaunty+wunder+kind+scm@eldergods.com>
2020-01-03 15:52:41 -0500

    disclaim that 'dict' is an unstable name

--- type.js
+++ type.js
@@ -1,4 +1,5 @@
-export const
+// UNSTABLE: name choice 'dict' does not feel right
+export const dict= {
 	block= "b", // block special file
 	character= "c", // character special file
 	directory= "d",
@@ -7,7 +8,7 @@ export const
 	link: "l", // symbolic link
 	socket: "s", // "socket"
 	door: "D" // solaris door
-
+}
 export function type( type){
 	if( !_type.value[ type]){
 		throw new Error(`Unexpected type '${ type}'`)
@@ -25,18 +26,19 @@ Object.defineProperties( type, {
 		value: {}
 	}
 }
-Object.entries({
-	block: block
-	character: character
-	directory: directory,
-	file: file,
-	pipe: pipe,
-	link: link,
-	socket: socket,
-	door: door
-}).forEach( function( key, value){
+Object.entries( dict).forEach( function( key, value){
 	type[ key]= value
 	type.name[ key]= value
 	type[ value]= key
 	type.value[ value]= key
 })
+
+export const
+	block: type( "block"),
+	character: type( "character"),
+	directory: type( "directory")
+	file: type( "file),
+	pipe: type( "pipe"),
+	link: type( "link"),
+	socket: type( "socket"),
+	door: type( "door")
