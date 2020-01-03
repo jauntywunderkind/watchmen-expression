3dc4e55f486d8e810588775feeab6e4aeee43c75
jauntywunderkind <jaunty+wunder+kind+scm@eldergods.com>
2020-01-03 17:42:20 -0500

    _helper.js

---
+++ _helper.js
@@ -0,0 +1,17 @@
+export { gitdir, Gitdir} from "./gitdir.js"
+export { somedir, Somedir} from "./somedir.js"
+export { subdir, Subdir} from "./subdir.js"
+export const helper= {
+	gitdir,
+	Gitdir,
+	somedir,
+	Somedir,
+	subdir,
+	Subdir
+}
+export {
+	helper as default,
+	helper as dict,
+	helper as Dict,
+	helper as Helper,
+}
